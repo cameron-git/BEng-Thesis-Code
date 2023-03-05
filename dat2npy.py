208d771b0e91a6ca1c1b6f18707173793e51560e
cameron-git <cameronbarkeran@gmail.com>
2023-03-05 01:36:03 +0000

    .gitignore data files

--- dat2npy.py
+++ dat2npy.py
@@ -6,6 +6,6 @@ from metavision_core.event_io import load_events
 
 os.chdir(sys.path[0])
 
-for f in glob.glob("./Line/*.dat"):
+for f in glob.glob("./data/Line/*.dat"):
     print(f)
     np.save(f[0:-4], load_events(f))
