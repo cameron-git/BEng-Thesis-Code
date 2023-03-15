ce94cd19d894784453efc153c712c5ab561d446e
cameron-git <cameronbarkeran@gmail.com>
2023-03-15 17:36:44 +0000

    15/03

--- models.py
+++ models.py
@@ -73,15 +73,33 @@ class Line2(torch.nn.Module):
 
         return torch.stack(outputs)
     
-class SNN2(torch.nn.Module):
+class Blur1(torch.nn.Module):
     def __init__(self):
         """
         - LIF 0.001
         - LI 0.002
         """
-        super(Line2, self).__init__()
+        super(Blur1, self).__init__()
         self.dt = 361e-6
-        self.l1 = snn.LIFCell(p=snn.LIFParameters(tau_mem_inv=1/0.001))
+        self.l1 = snn.LIFCell(p=snn.LIFParameters())
+        self.l2 = snn.LICell()
+
+        kernel = torch.tensor([[0, 0.15, 0],
+                               [0.15, 0.4, 0.15],
+                               [0, 0.15, 0]])
+        # if torch.cuda.is_available():
+        #     kernal = kernel.cuda(0)
+        convolution = torch.nn.Conv2d(
+            1,
+            1,
+            kernel_size,
+            padding=1,
+            bias=False,
+            dilation=1,
+        )
+        convolution.weight = torch.nn.Parameter(
+            kernel.unsqueeze(0).unsqueeze(0))
+        self.convolution = convolution
 
     def forward(self, x):
         seq_length, _, _, _ = x.shape
@@ -89,8 +107,11 @@ class SNN2(torch.nn.Module):
         outputs = []
 
         for ts in range(seq_length):
-            z = x[ts, :, :, :]  # .view(-1, self.input_features)
+            z = x[ts, :, :, :]
+            # z = self.convolution(z)
+            # z = self.convolution(z)
             z, s1 = self.l1(z, s1)
+            # z, s2 = self.l1(z, s2)
             outputs += [z]
 
         return torch.stack(outputs)
