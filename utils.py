f2a35937aa8de162e55c3e5188e1783aa06c5894
cameron-git <cameronbarkeran@gmail.com>
2023-03-27 16:47:08 +0100

    frame_merge added

--- utils.py
+++ utils.py
@@ -1,27 +1,46 @@
-#%%
 import numpy as np
-import glob
 import tonic
-import os
-import sys
-from metavision_core.event_io import load_events
-
-# %% dat2npy events
-
-os.chdir(sys.path[0])
-
-for f in glob.glob("./data/Line/*.dat"):
-    print(f)
-    np.save(f[0:-4], load_events(f))
+import torch
 
 
-# %% dat2npy frames
-os.chdir(sys.path[0])
-transform = tonic.transforms.ToFrame(
-    sensor_size=[480, 360, 1],
-    time_window=361,
-)
-for f in glob.glob("./data/Line/*.dat"):
-    print(f)
-    np.save(f[0:-4]+"_frames", transform(load_events(f)))
-# %%
+def n_evs(evs):
+    """
+    MNIST: 4172
+    POKERDVS: 2991
+    DVSGesture: 361903
+    ASLDVS: 28149
+    CIFAR10DVS: 204409
+    """
+    sum = 0
+    for i, ev in enumerate(evs):
+        # print(i)
+        sum = sum + (ev[0].size-sum)/(i+1)
+    print(sum)
+
+
+def t_evs(evs):
+    """
+    MNIST: 307663
+    POKERDVS: 17283
+    DVSGesture: 6455240
+    ASLDVS: 110247
+    CIFAR10DVS: 1293438
+    """
+    sum = 0
+    for i, ev in enumerate(evs):
+        # print(i)
+        sum = sum + (ev[0][-1][0]-sum)/(i+1)
+    print(sum)
+
+
+def frame_merge(in_frames, n):
+    """
+    n: number of frames per merge
+    """
+    shape = list(in_frames.shape)
+    shape[0] = int(shape[0]/n)
+    out_frames = torch.empty(shape)
+    for i in range(shape[0]):
+        j = i*n
+        out_frames[i] = torch.sum(in_frames[j:j+n], 0)
+    return out_frames
